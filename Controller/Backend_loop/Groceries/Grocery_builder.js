3d14fb5396fdebf0b4c1cdab0f61611efdf89537
Kowshik Eggoni <kowshik.eggoni@gmail.com>
2022-11-23 14:19:19 +0530

    Added Grocery Builder

---
+++ Controller/Backend_loop/Groceries/Grocery_builder.js
@@ -0,0 +1,62 @@
+Require User_diet (from the database)
+// You need to fetch user meal and quantity recommendations to prep the grocery list
+
+let freshProduceArray;
+let meatEggsArray;
+let fPQuantityArray;
+let mEQuantityArray;
+
+let freshProduceFinder = () => {
+    for (let j = 0; j < [recipe.ingredients].length; j++) {
+        if (ingredients.category === "GREEN LEAFY VEGETABLES"||"OTHER VEGETABLES"||"ROOTS, TUBERS, AND BULBS"||"FRUITS") {
+            push the ingredient to freshProduceArray();
+
+            (recommended quantity of recipe)/(serving size of recipe*total servings of recipe)*(quantity of the ingredient);
+            push the quantity to fPQuantityArray(); 
+        }
+    }
+}
+
+let meatEggsFinder = () => {
+    for (let j = 0; j < [recipe.ingredients].length; j++) {
+        if (ingredients.category === "MEAT"||"EGG AND EGG PRODUCTS") {
+            push the ingredient to meatEggsArray();
+    
+            (recommended quantity of recipe)/(serving size of recipe*total servings of recipe)*(quantity of the ingredient);
+            push the quantity to mEQuantityArray(); 
+        }
+    }
+}
+
+
+//Use this function to run through all the ingredients in a recipe and identify fruits, vegetables, meat, and eggs among them and their quantities.
+//Store them somewhere so that we can send them as push notifications and display as a user-specific checklist
+
+if (today = "Mon" || "Wed" ) {
+    run cron.job @ 12:01 AM
+    for (let i = 0; i < [Today+Tomorrow meals].length; i++) {
+        groceryFinder();
+    }
+//We are showing grocery list for two days for fruits and vegetables. Automatically generate these lists at 12:01 AM on Mondays and Wednesdays
+} else if (today = "Fri") {
+    run cron.job @ 12:01 AM
+    for (let i = 0; i < [Today+ Tomorrow+ Day after tomorrow meals].length; i++) {
+        groceryFinder(); 
+    }
+//On Fridays we generate the fruits and vegetables grocery lists for 3 days
+}
+
+if (today = "Mon") {
+    run cron.job @ 12:01 AM
+    for (let i = 0; i < [Today+Tomorrow meals].length; i++) {
+        meatEggsFinder();    
+    }
+//On Mondays we generate the fruits and vegetables grocery lists for 3 days
+}
+
+
+// Convert all the quantities to grams;
+// Remove duplicates and add up quantities;
+// Talk to front-end. I think they are fetching the recommendations dynamically. Display the results as follows;
+console.log("Fruits and Vegetables"/n, freshProduceArray[n], " ", fPQuantityArray[n], " grams"/n, meatEggsArray[n], " ", mEQuantityArray[n]);
+// n = 0 to n (number of ingredients that are matching the query); 
--- One_time_ops/Allergins_tagger.js
+++ One_time_ops/Allergins_tagger.js
@@ -2,12 +2,7 @@
 //When we generate diet for a person, we will use this list to screen recipes against their allergies 
 Require Recipes database
 //Write routes to fetch the Recipes collection from Wholesome Eten DB 
-Const Array = [{"Peanuts": "Groundnuts"},
-                {"Tree nuts": ["Almonds", "Cashews", "Pine nuts"]}
-                {"Allergin x": "Ingredient y"}]
-
-//This Array contains all the allergins in their respective categories. 
-//We compare all the recipes against this Array and generate the Allergins list for each recipe
+//Here, we compare all the recipes against the cases in the loop below and generate the Allergins list for each recipe
 //We will do this for all the recipes and store it in the DB.
 //Every time we add a new recipe, we will go through the same process. 
 //Also, we will do this once every month or so, just as a precautionary maintenance until the data standardises
@@ -18,15 +13,30 @@ for (let i=1; i<db.Recipes.length; i++) {
 // runs the loop through the entire ingredient list of a recipe
         let arr = 0;
         switch (Ingredient_name) {
-            case "Groundnuts":
-              arr = "Peanuts";
+            case ("Butter" || "Cheese" || "Buffalo milk" || "Cow milk" || "Paneer" || "Khoa"):
+              arr = "Milk";
               break;
-            case ("Almonds" || "Cashews" || "Pine nuts"):
+            case ("Egg" || "Egg white" || "Egg yolk" || "Boiled egg" || "Boiled egg white" || "Boiled egg yolk" || "Duck egg" || "Quail egg"):
+              arr = "Egg"; 
+              break;
+            case ("Almond" || "Cashew nut" || "Pistachio nuts" || "Walnut" || "Pine nuts"):
               arr = "Tree nuts";
               break;
-            case "Ingredient y":
-               day = "Allergin x";
+            case ("Ground nut"||"Peanut Powder"):
+              arr = "Peanut";
               break;
+            case ("Flax seeds"||"Niger seeds"||"Pine nuts"||"Pistachio nuts"||"Safflower seeds"||"Sunflower seeds"||"Watermelon seeds"||"Chia seeds"):
+              arr = "Seeds";
+              break;
+            case ("Barley" || "Refined wheat flour" || "Wheat flour" || "Wheat" || "Bulgur" || "Wheat rava" || "Wheat vermicelli" || "Roasted vermicelli"):
+              arr = "Gluten";
+             break;
+            case ("Anchovy"||"Bombay duck"||"Hilsa"||"Milk fish"||"White Pomfret"||"Red snapper"||"Salmon"||"Sardine"||"Shark"||"Tilapia"||"Tuna"||"Vanjaram"||"Cat fish"||"Catla"||"Freshwater Eel"||"Pangas"||"Rohu"):
+              arr = "Fish";
+             break;
+            case ("Sea crab"||"Lobster"||"Mud crab"||"Oyster"||"Tiger Prawns"||"Clam"||"Octopus"||"Squid"||"Crab"||"Big prawns"||"Small prawns"||"Tiger prawns"):
+              arr = "Shellfish";
+             break;
           } 
           return arr+=;
 //For all the true cases, the allergin category is added into arr (list of allergins) for each recipe 
@@ -34,4 +44,6 @@ for (let i=1; i<db.Recipes.length; i++) {
     console.log(Recipe_name, arr);
 //Once completed, the Recipe_name and arr are printed. The loop then moves on to the next recipe
 }
+
+Allergens
 //Once this code runs through the entire Recipe collection and generates the allergins for each recipe, we wil use DB query methods to append or update Allergins for each recipe
